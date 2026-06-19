d092855f2506a0155233d657a41e85a3a17e377b
Chinmay jaiswal <chinmay1000@gmail.com>
2026-06-19 17:59:24 +0530

    user repo

---
+++ src/main/java/com/chinmay/user_service_api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.chinmay.user_service_api.repository;
+
+import com.chinmay.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+    boolean existsByAadhaar(String aadhaar);
+    boolean existsByPan(String pan);
+}
