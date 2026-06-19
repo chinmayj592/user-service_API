0be6958b88ad23bb438ad878965dd8041717918b
Chinmay jaiswal <chinmay1000@gmail.com>
2026-06-19 18:01:41 +0530

    user service Impl

---
+++ src/main/java/com/chinmay/user_service_api/service/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package com.chinmay.user_service_api.service;
+
+import com.chinmay.userservice.dto.UserRequestDTO;
+import com.chinmay.userservice.dto.UserResponseDTO;
+import com.chinmay.userservice.entity.User;
+import com.chinmay.userservice.exception.ResourceNotFoundException;
+import com.chinmay.userservice.mapper.UserMapper;
+import com.chinmay.userservice.repository.UserRepository;
+import com.chinmay.userservice.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserResponseDTO create(UserRequestDTO dto) {
+
+        if (repository.existsByEmail(dto.email()))
+            throw new IllegalArgumentException("Email already exists");
+
+        if (repository.existsByAadhaar(dto.aadhaar()))
+            throw new IllegalArgumentException("Aadhaar already exists");
+
+        if (repository.existsByPan(dto.pan()))
+            throw new IllegalArgumentException("PAN already exists");
+
+        User user = UserMapper.toEntity(dto);
+        return UserMapper.toResponse(repository.save(user));
+    }
+
+    @Override
+    public UserResponseDTO update(Long id, UserRequestDTO dto) {
+        User user = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        UserMapper.updateEntity(user, dto);
+        return UserMapper.toResponse(repository.save(user));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<UserResponseDTO> getAll(Pageable pageable) {
+        return repository.findAll(pageable)
+                .map(UserMapper::toResponse);
+    }
+
+    @Override
+    public void delete(Long id) {
+        User user = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        repository.delete(user);
+    }
+}
